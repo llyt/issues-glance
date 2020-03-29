9519b300ca2c349d757c38271aa5a85010d0742d
anton@lytkin.me <anton@lytkin.me>
2020-03-29 13:15:07 +0300

    Added reselect in issuesList selector

--- src/store/issues/selectors.js
+++ src/store/issues/selectors.js
@@ -1,15 +1,22 @@
-export const getIssuesList = (state) => {
-  const { perPage, page, fetchedIssues } = state.issues
-  const startIndex = perPage * (page - 1)
-  const endIndex = perPage * page
-
-  return fetchedIssues.slice(startIndex, endIndex)
-}
+import { createSelector } from 'reselect'
 
 export const getError = (state) => state.issues.error
 export const getLoadingState = (state) => state.issues.isLoading
 export const getPage = (state) => state.issues.page
 export const getPerPage = (state) => state.issues.perPage
+export const getFetchedIssues = (state) => state.issues.fetchedIssues
 export const getIssuesTotalCount = (state) => state.issues.issuesTotalCount
 export const getPointerRepository = (state) => state.issues.pointerRepository
 export const getFetchedPages = (state) => state.issues.fetchedPages
+
+export const getIssuesList = createSelector(
+  getPage,
+  getPerPage,
+  getFetchedIssues,
+  (page, perPage, fetchedIssues) => {
+    const startIndex = perPage * (page - 1)
+    const endIndex = perPage * page
+
+    return fetchedIssues.slice(startIndex, endIndex)
+  }
+)
